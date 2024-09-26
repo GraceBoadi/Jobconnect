29eca94e5b69b2c301e1f84df1019981f574f6d2
GraceBoadi <100855666+GraceBoadi@users.noreply.github.com>
2024-09-26 20:36:10 +0000

    server index file updated

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
-import path from "../client/dist/index.html";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";  // Import path to handle file paths
+
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
@@ -13,7 +12,6 @@ import applicationRoute from "./routes/application.route.js";
 dotenv.config({});
 
 const app = express();
-const __dirname = path.resolve();  // Only required if using ES6 modules
 
 // middleware
 app.use(express.json());
@@ -22,7 +20,7 @@ app.use(cookieParser());
 app.use("/files", express.static("files"));
 
 const corsOptions = {
-  origin: "https://jobconnect-1.onrender.com",  // Change this to your frontend URL in production
+  origins: "https://jobconnect-1.onrender.com", // Change this to your frontend URL in production
   credentials: true,
 };
 
@@ -33,14 +31,6 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
-
-// Serve the Vite build files from client/dist
-app.use(express.static(path.join(__dirname, "client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
-});
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
