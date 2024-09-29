6fa458879f91b61447f461b9a38b0301f1e831fd
GraceBoadi <100855666+GraceBoadi@users.noreply.github.com>
2024-09-29 16:24:43 +0000

    server

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use("/files", express.static("files"));
 
 const corsOptions = {
-  origins: "http://localhost:3000/", // Change this to your frontend URL in production
+  origins: "*", // Change this to your frontend URL in production
   credentials: true,
 };
 
